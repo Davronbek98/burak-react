879d53539d686e822429eb5891b0e33fd3e3e688
Davronbek98 <yoldashevdavron1998@gmail.com>
2024-10-06 18:42:59 +0900

    feat: create routing system

--- src/index.tsx
+++ src/index.tsx
@@ -6,15 +6,18 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
-import "./css/index.css";
 import theme from "./app/MaterialTheme";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./css/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <App />
+        <Router>
+          <App />
+        </Router>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>,
