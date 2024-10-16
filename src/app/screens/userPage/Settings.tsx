2b1857efa1bb5b641bc44aee4e8dc6022f1a10fc
Davronbek98 <yoldashevdavron1998@gmail.com>
2024-10-16 13:22:10 +0900

    feat: publish helpPage screen component

--- src/app/screens/userPage/Settings.tsx
+++ src/app/screens/userPage/Settings.tsx
@@ -37,7 +37,7 @@ export function Settings() {
             className={"spec-input mb-phone"}
             type="text"
             placeholder={"no phone"}
-            value={"821024694424"}
+            value={"821024694422"}
             name="memberPhone"
           />
         </div>
